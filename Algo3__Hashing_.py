56c119b7a994bb5cccbfabbf8d5bf035ec17cd67
alex999ar <alexandrosaristovoulos@gmail.com>
2021-04-09 00:11:43 +0300

    increase speed

--- Algo3__Hashing_.py
+++ Algo3__Hashing_.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import sys
+import time
 
 ######################### Global Variables #########################
 #my seed
@@ -13,9 +14,9 @@ LOWEST_PRICE = 20
 HIGHEST_PRICE = 100
 
 #hash table details
-TOTAL_CARD_VISITS = 20
+TOTAL_CARD_VISITS = 1000000
 #(before I use it the program changes it to the nearest prime number)
-INITIAL_CAPACITY = 5
+INITIAL_CAPACITY = 1000
 MAX_LOAD_FACTOR = 0.6
 
 ######################### Functions #########################
@@ -37,7 +38,7 @@ def createCard():
     #pick a random value between 20 and 100 for the price they paid
     value = np.random.randint(LOWEST_PRICE, HIGHEST_PRICE)
 
-    #pick a random day (1-7 for Monday-Sunday)
+    #pick a random day (0-6 for Monday-Sunday)
     day = np.random.randint(0, 6)
 
     return cardDigits, value, day
@@ -139,63 +140,153 @@ def exceedLoadFactror(hashTable):
     if cardCount / len(hashTable) > MAX_LOAD_FACTOR:
         return True
     return False
-    
+
+def createFinalHashTable():
+    print("Creating hash table for {} card visits and a load factor of {}" .format(TOTAL_CARD_VISITS, MAX_LOAD_FACTOR))
+    #create an empty the hash table    
+    hashTable = [None] * findClosestPrime(INITIAL_CAPACITY)
+    print("The initial size of the hash table is: {}" .format(len(hashTable)))
+
+    #fill the table with cards
+    for i in range (TOTAL_CARD_VISITS):
+        #get all the values for a card
+        id, val, day = createCard()
+        #create the card
+        card = Card(id, val, day)
+        #put the card in the table
+        hashTable = putInHashTable(card, hashTable)
+
+        #if the table is too filled then create a bigger one
+        #and move everything to it
+        if i >= MAX_LOAD_FACTOR*len(hashTable):
+            if exceedLoadFactror(hashTable):        
+                hashTable = redefineHashTable(hashTable)
+                print("The new size of the hash table is: {}" .format(len(hashTable)))
+
+    return hashTable
+
+def getStats(hashTable):
+    #set the min payment value to the max of the system so that we definately find a smaller number in a card
+    minPaymentsCard = Card('', sys.maxsize, [])
+    #in the card with the max visits we put an empty list to change it once we find another card
+    maxVisitsCard = Card('', 0, None)
+    #create a dictionary with the visits of each day
+    dayVisits = 6 * [0]
+    #total number of conflicts
+    conflictsNum = 0
+
+    for i in range(len(hashTable)):
+        #first check if we have a card
+        if type(hashTable[i]) == Card:
+
+            #to get the card with the least amount payed
+            if hashTable[i].val < minPaymentsCard.val:
+                minPaymentsCard = hashTable[i]
+
+            #to get the card with the most visits
+            if len(hashTable[i].days) > len(maxVisitsCard.days) or maxVisitsCard.days == [None]:
+                maxVisitsCard = hashTable[i]
+
+            #update the visits for each day
+            for j in range(len(hashTable[i].days)):
+                dayVisits[hashTable[i].days[j]] += 1
+
+            #if the hash of the card doesn't match its position in the list it means that we had a conflict
+            if hash(hashTable[i].key)%len(hashTable) != i:
+                conflictsNum += 1
+
+    return minPaymentsCard, maxVisitsCard, dayVisits, conflictsNum
+
 
 ######################### Main #########################
-#create an empty the hash table    
-hashTable = [None] * findClosestPrime(INITIAL_CAPACITY)
-print("The initial size of the hash table is: {}" .format(len(hashTable)))
+startTime = time.time()
+hashTable = createFinalHashTable()
+endTime = time.time()
+totalTime = endTime - startTime
+print("Total time to create a hash table for {} cards with a load factor of {} is {} seconds" .format(TOTAL_CARD_VISITS, MAX_LOAD_FACTOR, totalTime))
 
-#fill the table with cards
-for i in range (TOTAL_CARD_VISITS):
-    #get all the values for a card
-    id, val, day = createCard()
-    #create the card
-    card = Card(id, val, day)
-    #put the card in the table
-    hashTable = putInHashTable(card, hashTable)
-
-    #if the table is too filled then create a bigger one
-    #and move everything to it
-    if exceedLoadFactror(hashTable):        
-        hashTable = redefineHashTable(hashTable)
-        print("The new size of the hash table is: {}" .format(len(hashTable)))
-
-print(hashTable)
+minPaymentsCard, maxVisitsCard, dayVisits, conflictsNum = getStats(hashTable)
+print("The card with the least amount payed is {} with {} payed " .format(minPaymentsCard.key, minPaymentsCard.val))
+print("The card with the most visits is {} with {} visit(s) " .format(maxVisitsCard.key, len(maxVisitsCard.days)))
+print("The day with the least visits is {} with {} visit(s) " .format(dayVisits.index(min(dayVisits)), min(dayVisits)))
+print("The total number of conflicts is {}\n" .format(conflictsNum))
 
 
-#variables to answer the questions
-#set the min payment value to the max of the system so that we definately find a smaller number in a card
-minPaymentsCard = Card('', sys.maxsize, [])
-#in the card with the max visits we put an empty list to change it once we find another card
-maxVisitsCard = Card('', 0, None)
-#create a dictionary with the visits of each day
-dayVisits = 7 * [0]
+#calculate time for double the card visits
+TOTAL_CARD_VISITS = 2*TOTAL_CARD_VISITS
+
+startTime = time.time()
+hashTable = createFinalHashTable()
+endTime = time.time()
+totalTime = endTime - startTime
+print("Total time to create a hash table for {} cards with a load factor of {} is {} seconds" .format(TOTAL_CARD_VISITS, MAX_LOAD_FACTOR, totalTime))
+minPaymentsCard, maxVisitsCard, dayVisits, conflictsNum = getStats(hashTable)
+print("The card with the least amount payed is {} with {} payed " .format(minPaymentsCard.key, minPaymentsCard.val))
+print("The card with the most visits is {} with {} visit(s) " .format(maxVisitsCard.key, len(maxVisitsCard.days)))
+print("The day with the most visits is {} with {} visit(s) " .format(dayVisits.index(max(dayVisits)), max(dayVisits)))
+print("The total number of conflicts is {}\n" .format(conflictsNum))
+
+
+#calculate conflicts for the original number of card visits but for a load factor of 0.7
+TOTAL_CARD_VISITS = int(TOTAL_CARD_VISITS/2)
+MAX_LOAD_FACTOR = 0.7
+hashTable = createFinalHashTable()
 #total number of conflicts
 conflictsNum = 0
 
 for i in range(len(hashTable)):
     #first check if we have a card
     if type(hashTable[i]) == Card:
-
-        #to get the card with the least amount payed
-        if hashTable[i].val < minPaymentsCard.val:
-            minPaymentsCard = hashTable[i]
-
-        #to get the card with the most visits
-        if len(hashTable[i].days) >= len(maxVisitsCard.days) or maxVisitsCard.days == [None]:
-            maxVisitsCard = hashTable[i]
-
-        #update the visits for each day
-        for j in range(len(hashTable[i].days)):
-            dayVisits[hashTable[i].days[j]] += 1
-
         #if the hash of the card doesn't match its position in the list it means that we had a conflict
         if hash(hashTable[i].key)%len(hashTable) != i:
             conflictsNum += 1
 
+print("The total number of conflicts is {} \n" .format(conflictsNum))
 
-print("The card with the least amount payed is {} with {} payed " .format(minPaymentsCard.key, minPaymentsCard.val))
-print("The card with the most visits is {} with {} visit(s) " .format(maxVisitsCard.key, len(maxVisitsCard.days)))
-print("The day with the most visits is {} with {} visit(s) " .format(dayVisits.index(max(dayVisits)), max(dayVisits)))
+#calculate conflicts for the original number of card visits but for a load factor of 0.8
+MAX_LOAD_FACTOR = 0.8
+hashTable = createFinalHashTable()
+#total number of conflicts
+conflictsNum = 0
+
+for i in range(len(hashTable)):
+    #first check if we have a card
+    if type(hashTable[i]) == Card:
+        #if the hash of the card doesn't match its position in the list it means that we had a conflict
+        if hash(hashTable[i].key)%len(hashTable) != i:
+            conflictsNum += 1
+
 print("The total number of conflicts is {}" .format(conflictsNum))
+
+#calculate conflicts for 2*original number of card visits and a load factor of 0.7
+TOTAL_CARD_VISITS = TOTAL_CARD_VISITS*2
+MAX_LOAD_FACTOR = 0.7
+hashTable = createFinalHashTable()
+#total number of conflicts
+conflictsNum = 0
+
+for i in range(len(hashTable)):
+    #first check if we have a card
+    if type(hashTable[i]) == Card:
+        #if the hash of the card doesn't match its position in the list it means that we had a conflict
+        if hash(hashTable[i].key)%len(hashTable) != i:
+            conflictsNum += 1
+
+print("The total number of conflicts is {} \n" .format(conflictsNum))
+
+#calculate conflicts for 2*original number of card visits and a load factor of 0.8
+MAX_LOAD_FACTOR = 0.8
+hashTable = createFinalHashTable()
+#total number of conflicts
+conflictsNum = 0
+
+for i in range(len(hashTable)):
+    #first check if we have a card
+    if type(hashTable[i]) == Card:
+        #if the hash of the card doesn't match its position in the list it means that we had a conflict
+        if hash(hashTable[i].key)%len(hashTable) != i:
+            conflictsNum += 1
+
+print("The total number of conflicts is {}" .format(conflictsNum))
+
+
